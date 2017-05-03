ebbfc3c82ec8e816834ba63a9b7121d2e5eb9c76
Stephen Crawley <crawley.stephen@gmail.com>
2017-05-03 21:27:18 +1000

    2 fixes + some polishing

--- extract/Projects.py
+++ extract/Projects.py
@@ -19,46 +19,38 @@ class Projects(Usages):
         Usages.build_parser(parser, func)
         
     def check_args(self, args):
-        Usages.check_args(self, args)        
+        Usages.check_args(self, args)
+        if args.legacy and not args.csv:
+            sys.stderr.write('The --legacy only supported with --csv\n')
+            sys.exit(1)
+
 
     def run(self, args):
         self.setup_nova()
         self.setup_keystone()
         self.fetch_usage(args)
-        if args.legacy:
-            headings = ["tenant_id", "tenant_name",
-                        "instance_count", "instance_hours", "vcpu_hours",
-                        "memory_hours_mb", "disk_hours_gb"]
-            usage = map(lambda u: [u.tenant_id,
-                                   self.projects[u.tenant_id].name \
-                                   if u.tenant_id in self.projects else None,
-                                   len(u.server_usages),
-                                   u.total_hours,
-                                   u.total_vcpus_usage,
-                                   u.total_memory_mb_usage,
-                                   u.total_local_gb_usage],
-                        self.raw_usage)
-            
-        else:
-            headings = ["nu_year", "nu_month", "tenant_id", "tenant_name",
-                        "instance_count", "instance_hours", "vcpu_hours",
-                        "memory_hours_mb", "disk_hours_gb"]
-            usage = map(lambda u: [self.year,
-                                   self.month,
-                                   u.tenant_id,
-                                   self.projects[u.tenant_id].name \
-                                   if u.tenant_id in self.projects else None,
-                                   len(u.server_usages),
-                                   u.total_hours,
-                                   u.total_vcpus_usage,
-                                   u.total_memory_mb_usage,
-                                   u.total_local_gb_usage],
-                        self.raw_usage)
-            
+        headings = ["tenant_id", "tenant_name",
+                    "instance_count", "instance_hours", "vcpu_hours",
+                    "memory_hours_mb", "disk_hours_gb"]
+        period = []
+        if not args.legacy:
+            headings = ["nu_year", "nu_month"] + headings
+            period = [self.year, self.month]
+        usage = map(lambda u: period + \
+                    [u.tenant_id,
+                     self.projects[u.tenant_id].name \
+                     if u.tenant_id in self.projects else None,
+                     len(u.server_usages),
+                     u.total_hours,
+                     u.total_vcpus_usage,
+                     u.total_memory_mb_usage,
+                     u.total_local_gb_usage],
+                    self.raw_usage)
+        
         if args.csv:
             self.csv_output(headings, usage, filename=args.filename)
         else:
             self.db_insert(headings, usage,
                            args.tablename or "nectar_usage",
-                           replaceAll=True)
+                           replaceAll=False)
 
