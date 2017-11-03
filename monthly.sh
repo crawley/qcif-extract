f82939db20d72a900200eb80d5d3b015b03cb539
Stephen Crawley <crawley.stephen@gmail.com>
2017-11-03 16:18:13 +1000

    Stopped using the legacy managers table ...

--- monthly.sh
+++ monthly.sh
@@ -11,7 +11,7 @@ MONTH=$2
 
 set -x
 extract/extract.py $FLAGS general \
-&& extract/extract.py $FLAGS managers --legacy \
+&& extract/extract.py $FLAGS managers \
 && extract/extract.py $FLAGS project-usage --year $YEAR --month $MONTH \
 && extract/extract.py $FLAGS instance-usage --year $YEAR --month $MONTH \
 && extract/extract.py $FLAGS instance-usage --qriscloud --year $YEAR --month $MONTH 
